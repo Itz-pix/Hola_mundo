8d23b280aad4047f13c838f34a41795903e09990
Itz-pix <caai090628mgthvta8@cbtis217.edu.mx>
2025-11-22 16:21:43 -0600

    segunda version del programa

--- EncuentraOcho.ccp.cpp
+++ EncuentraOcho.ccp.cpp
@@ -3,17 +3,18 @@ using namespace std;
 
 int main()
 {
+    //Numeros que van a ingresar
     int N;
 
     cin >> N;
-
+    //Analizar cada numero
     int arreglo[N];
-
+    //lectura de los numeros
     for (int i = 0; i < N; i++)
         cin >> arreglo[i];
-
+    //Comparacion para encontrar el 8
     for (int i = 0; i < N; i++)
-        if (arreglo[i] == 4) {
+        if (arreglo[i] == 8) {
             cout << 1;
             return 0;
         }
